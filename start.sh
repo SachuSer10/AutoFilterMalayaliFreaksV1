a6ce8290752872fa431f7b357528e8b1d270f3d0
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-08-21 00:12:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/SachuSer10/MFAutoGilterv3 /MFAutoGilterv3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
