2a29f90a405e722f64ab2b9feab19b821e5b4bb4
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-08-21 00:59:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,7 +13,15 @@ class script(object):
 
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
+- This Is A Closed Source Project . 
 
+- Base Repo - https://github.com/EvamariaTG/EvaMaria  
+
+<b>MAIN CHANNEL:</b>
+
+- <a href=https://t.me/MovieFlix_TG>MOVIEFLIX</a>"""
+
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 
 
