82ace561b5d466cb3c10c66d02849a168cebb23e
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-08-22 15:44:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,7 +44,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🌀 Owner', url='https://t.me/MichealAnjoottiTG')
             InlineKeyboardButton('🤖 Updates', url='https://t.me/MovieFlix_TG')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
@@ -90,7 +90,9 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('⭐️ 🔍 Search ', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🌀 Owner', url='https://t.me/MichealAnjoottiTG')
+                              
+                               
             InlineKeyboardButton('🤖 Updates', url='https://t.me/MovieFlix_TG')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
