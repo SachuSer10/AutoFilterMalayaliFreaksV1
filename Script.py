5e824bc110b39dd2765e5d169994aef880ca8ed7
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-08-22 16:00:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -105,7 +105,7 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+    STATUS_TXT = """ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ ➪ : <code>{}</code>
 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ ➪ : <code>{}</code>
 ᴛᴏᴛᴀʟ ᴄʜᴀᴛꜱ ➪ : <code>{}</code>
 ᴜꜱᴇᴅ ꜱᴛᴏʀᴀɢᴇ ➪: <code>{}</code> 𝙼𝚒𝙱
