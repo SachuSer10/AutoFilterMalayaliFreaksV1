a74f08ee55d3a7f725217dc6d69b4ef103ced434
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-09-15 21:20:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -447,19 +447,19 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🔮 Status', callback_data='stats')
         ]]
         reply1 = await query.message.reply_text(
-            text="â–¢â–¢â–¢"
+            text="⦿"
         )
         await asyncio.sleep(0.5)
         reply2 = await reply1.edit_text(
-            text="â–£â–¢â–¢"
+            text="⦿⦿"
         )
         await asyncio.sleep(0.5)
         reply3 = await reply2.edit_text(
-            text="â–£â–£â–¢"
+            text="⦿⦿⦿"
         )
         await asyncio.sleep(0.5)
         reply4 = await reply3.edit_text(
-            text="â–£â–£â–£"
+            text="⦿⦿⦿⦿"
         )
         await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
