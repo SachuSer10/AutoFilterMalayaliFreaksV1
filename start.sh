d51ef2142d664b3bf640dbd13fb65cb97916e906
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-09-26 22:12:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SachuSer10/AutoFilterMalayaliFreaksV1
+  git clone https://github.com/SachuSer10/AutoFilterMalayaliFreaksV1 /AutoFilterMalayaliFreaksV1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterMalayaliFreaksV1
