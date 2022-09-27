ee80ce92f81613afeb27bbc74277dfbec774a77e
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-09-27 07:44:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterMalayaliFreaksV1 
 fi
-cd /MFAutoGilterv3 
+cd /AutoFilterMalayaliFreaksV1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
